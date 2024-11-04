cf01583c2ddb1984436d9b54c98e2a7987f64f45
Zastera <Dustin.Zastera@Donaldson.com>
2024-11-04 06:54:24 -0600

    Patched to ensure unique filenames during attachment save.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // is_rtf
 bool is_rtf(RawVector v);
 RcppExport SEXP _msgxtractr_is_rtf(SEXP vSEXP) {
